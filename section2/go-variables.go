4071b838be8f2dbad9828e674f5a119eeca9d5fe
lordghostx <solomonghost9@gmail.com>
2020-07-13 23:07:10 +0100

    variables

---
+++ section2/go-variables.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+  var var1 int
+  var1 = 5
+
+  var var2 = 2
+
+  var (
+    var3 = 3
+    var4 = 7
+    var5 = 6
+  )
+
+  var6 := 10
+  var7 := 5
+
+  fmt.Println(var1 + var2)
+  fmt.Println(var3 + var4 + var5)
+  fmt.Println(var6 + var7)
+}
