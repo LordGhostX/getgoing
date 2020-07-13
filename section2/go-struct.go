bd98ccb7d9b4203d34a25ac42d4ec9a20028c3a2
lordghostx <solomonghost9@gmail.com>
2020-07-13 23:42:02 +0100

    structs

---
+++ section2/go-struct.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+  "fmt"
+)
+
+type Car struct {
+  Name  string
+  Age int
+  ModelNo int
+}
+
+func (c Car) Print() {
+  fmt.Println(c)
+}
+
+func (c Car) Drive() {
+  fmt.Println("Driving...")
+}
+
+func main() {
+  car1 := Car {"Ford", 10, 10001}
+  car2 := Car {
+    Name: "Toyota",
+    Age: 15,
+    ModelNo: 23450,
+  }
+
+  fmt.Println(car1)
+  fmt.Println(car2)
+
+  car1.Print()
+  car2.Drive()
+}
