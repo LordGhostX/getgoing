53f953ad3d5007a7df6540c1c333ad0ad2403f55
lordghostx <solomonghost9@gmail.com>
2020-07-13 23:07:47 +0100

    arrays

---
+++ section2/go-arrays.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+  "fmt"
+)
+
+func todo() {
+  arr1 := [] int {1, 2, 3, 4}
+  arr2 := [] string {"my", "name", "is"}
+
+  arr2 = append(arr2, "ghost", "!!!")
+
+  fmt.Println(arr1)
+  fmt.Println(arr2)
+}
+
+func main() {
+  todo()
+}
