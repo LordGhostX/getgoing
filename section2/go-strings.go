8500bf402995fbf7f507c67467c336ce4210376a
lordghostx <solomonghost9@gmail.com>
2020-07-13 23:07:38 +0100

    strings

---
+++ section2/go-strings.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+  "fmt"
+  "strings"
+)
+
+func main() {
+  var var1 string
+  var1 = "my name"
+
+  var2 := "my second name"
+
+  fmt.Println(var1)
+  fmt.Println(var2)
+  fmt.Println(strings.Contains(var2, "name"))
+  fmt.Println(strings.ReplaceAll(var1, "m", "n"))
+  fmt.Println(strings.Split(var2, "m"))
+  fmt.Println(var1 + " " + var2)
+  fmt.Println(var1, var2)
+}
