7f5cdf072ccc6430dc1e6731b84b4675ab9e8213
lordghostx <solomonghost9@gmail.com>
2020-07-17 18:46:05 +0100

    added lambda & wait groups

---
+++ section3/goroutine-2.go
@@ -0,0 +1,24 @@
+package main
+import (
+  "fmt"
+  "sync"
+)
+
+func main() {
+  wg := &sync.WaitGroup{}
+  wg.Add(2)
+
+  go func () {
+    fmt.Println("Hello")
+    wg.Done()
+  }()
+
+  go func () {
+    fmt.Println("World!")
+    wg.Done()
+  }()
+
+  fmt.Println("Start...")
+  wg.Wait()
+  fmt.Println("End...")
+}
