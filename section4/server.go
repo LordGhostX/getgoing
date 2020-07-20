082949569d91007c814732d82a55da84cef33a3a
lordghostx <solomonghost9@gmail.com>
2020-07-20 07:17:05 +0100

    go http server

---
+++ section4/server.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+  "net/http"
+  "fmt"
+)
+
+func main() {
+  mux := http.NewServeMux()
+  mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+    w.Write([]byte("Hello World"))
+  })
+
+  fmt.Println("Listening...")
+  http.ListenAndServe("localhost:3000", mux)
+}
